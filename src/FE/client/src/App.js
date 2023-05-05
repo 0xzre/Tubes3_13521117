e180f03a1e5f6c0688578a8b05eb490349e40017
blixa-rd <16521074@mahasiswa.itb.ac.id>
2023-05-05 16:32:27 +0700

    feat: handle sync response and empty prompt

--- src/FE/client/src/App.js
+++ src/FE/client/src/App.js
@@ -18,134 +18,143 @@ function App() {
   }
   ]);
 
-  function handleSubmit(e){
+  async function handleSubmit(e){
     e.preventDefault();
     setChatLog(prevLog => [...prevLog, { user: "me", message: `${input}` }]);
     setInput("");
     console.log("Input sent:");
+    if (input === "") {
+      setChatLog(prevLog => [...prevLog, { user: "gpt", message: `Masukkan input!` }]);
+      return
+    }
 
     // Check if there is more than 1 sentences with delimiter ". "
     if (input.includes(". ")){
       const sentences = input.split(". ").map(sentence => sentence.trim());
-      sentences.forEach((sentence) => {
+      for (let i = 0; i < sentences.length; i++) {
+        const sentence = sentences[i];
         console.log(sentence);
-        getAnswer(sentence);
-      });
-    } else { // Only 1 sentence
-      getAnswer(input);
+        await getAnswer(sentence);
     }
   }
+
+  // Only 1 sentence
+  else {
+    await getAnswer(input);
+  }
   
-  function getAnswer(question){
-    var encodedInput = encodeURIComponent(question);
+async function getAnswer(question){
+      var encodedInput = encodeURIComponent(question);
     
-    //TODO
-    //MAKE TOGGLE KMP AND BM
-    //if toggle KMP
-    var url = `/response/KMP/${encodedInput}`;
-    
-    //if toggle BM
-    // var url = `/response/BM/${encodedInput}`;
+      //TODO
+      //MAKE TOGGLE KMP AND BM
+      //if toggle KMP
+      var url = `/response/KMP/${encodedInput}`;
+      
+      //if toggle BM
+      // var url = `/response/BM/${encodedInput}`;
 
-    axios.get(url, {
-      responseType: 'json'
-    }).then(response => {
-      if(response.status === 200) {
-        if (response.data == null) {
-          setChatLog(prevLog => [...prevLog, { user: "gpt", message: "Pertanyaan tidak ditemukan, silakan tambahkan pertanyaan"}])
-          console.log("Data not found");
-        }
-
-        else {
-          // response.data is already json format
-          if (response.data[0].answer != null){
-
-            // Suggestion to user
-            if (response.data[0].answer === "Pertanyaan tidak ditemukan, mungkin maksudnya:") {
-              var suggestions = response.data[0].answer + `<br/>`;
-
-              for (let i = 1; i < response.data.length; i++) {
-                var capitalizeSuggestion = response.data[i].question.charAt(0).toUpperCase() + response.data[i].question.slice(1);
-                var rowSuggestion = i.toString() + ". " + capitalizeSuggestion;
-                suggestions = suggestions + rowSuggestion;
-
-                if (i < response.data.length - 1) {
-                  suggestions = suggestions + `<br/>`
-                }
-              }
-              setChatLog(prevLog => [...prevLog, { user: "gpt", message: suggestions }]);
-              console.log("Get response from listing suggestions");
-            }
-
-            else { // Other usage
-
-              // Json data more than 1 (not only flag, used for listing questions)
-              if (response.data.length > 1) {
-                var questionList = response.data[0].answer + '<br/>'
-
-                for (let i = 1; i < response.data.length; i++) {
-                  var capitalizeList = response.data[i].question.charAt(0).toUpperCase() + response.data[i].question.slice(1);
-                  var rowList = i.toString() + ". " + capitalizeList;
-                  questionList = questionList + rowList;
-
-                  if (i < response.data.length - 1){
-                    questionList = questionList + `<br/>`;
-                  }
-                }
-                setChatLog(prevLog => [...prevLog, { user: "gpt", message: questionList }]);
-                console.log("Get response from listing questions");
-              }
-              
-              // Casual case
-              else {
-                var gptResponse = response.data[0].answer;
-                gptResponse = gptResponse.charAt(0).toUpperCase() + gptResponse.slice(1);
-                setChatLog(prevLog => [...prevLog, { user: "gpt", message: gptResponse }]);
-                console.log("Get response from the most normal case");
-              }
-            }
+      axios.get(url, {
+        responseType: 'json'
+      }).then(response => {
+        if(response.status === 200) {
+          if (response.data == null) {
+            setChatLog(prevLog => [...prevLog, { user: "gpt", message: "Pertanyaan tidak ditemukan, silakan tambahkan pertanyaan"}])
+            console.log("Data not found");
           }
 
-          else { // response.data is in array of map
-            console.log("Check response from array of map");
+          else {
+            // response.data is already json format
+            if (response.data[0].answer != null){
 
-            // If array invalid as a map (usually as a text)
-            if (response.data[0]["answer"] == null){
-              var dataStr = "";
+              // Suggestion to user
+              if (response.data[0].answer === "Pertanyaan tidak ditemukan, mungkin maksudnya:") {
+                var suggestions = response.data[0].answer + `<br/>`;
 
-              // If array have leading number (ex: 1[{answer: blabla}])
-              // Remove leading number and parse into json
-              if (!response.data.startsWith('[')){
-                dataStr = response.data.substring(response.data[0]);
-                const data = JSON.parse(dataStr);
-                setChatLog(prevLog => [...prevLog, { user: "gpt", message: data[0]["answer"]}]);
-                console.log("Get response from parsing leading number");
+                for (let i = 1; i < response.data.length; i++) {
+                  var capitalizeSuggestion = response.data[i].question.charAt(0).toUpperCase() + response.data[i].question.slice(1);
+                  var rowSuggestion = i.toString() + ". " + capitalizeSuggestion;
+                  suggestions = suggestions + rowSuggestion;
+
+                  if (i < response.data.length - 1) {
+                    suggestions = suggestions + `<br/>`
+                  }
+                }
+                setChatLog(prevLog => [...prevLog, { user: "gpt", message: suggestions }]);
+                console.log("Get response from listing suggestions");
               }
-              
-              // Array map more than 1, need to combined first (update question)
-              else {
-                const combinedArray = response.data.replace(/\]\[/g, ',');
-                const parsedArray = JSON.parse(combinedArray);
-                setChatLog(prevLog => [...prevLog, { user: "gpt", message: parsedArray[0]["answer"]}]);
-                console.log("Get response from parsing more than 1 array map");
+
+              else { // Other usage
+
+                // Json data more than 1 (not only flag, used for listing questions)
+                if (response.data.length > 1) {
+                  var questionList = response.data[0].answer + '<br/>'
+
+                  for (let i = 1; i < response.data.length; i++) {
+                    var capitalizeList = response.data[i].question.charAt(0).toUpperCase() + response.data[i].question.slice(1);
+                    var rowList = i.toString() + ". " + capitalizeList;
+                    questionList = questionList + rowList;
+
+                    if (i < response.data.length - 1){
+                      questionList = questionList + `<br/>`;
+                    }
+                  }
+                  setChatLog(prevLog => [...prevLog, { user: "gpt", message: questionList }]);
+                  console.log("Get response from listing questions");
+                }
+                
+                // Casual case
+                else {
+                  var gptResponse = response.data[0].answer;
+                  gptResponse = gptResponse.charAt(0).toUpperCase() + gptResponse.slice(1);
+                  setChatLog(prevLog => [...prevLog, { user: "gpt", message: gptResponse }]);
+                  console.log("Get response from the most normal case");
+                }
               }
             }
 
-            else { // Valid array, directly retrieve the data
-              setChatLog(prevLog => [...prevLog, { user: "gpt", message: response.data[0]["answer"]}]);
-              console.log("Get Response from direct valid array map");
+            else { // response.data is in array of map
+              console.log("Check response from array of map");
+
+              // If array invalid as a map (usually as a text)
+              if (response.data[0]["answer"] == null){
+                var dataStr = "";
+
+                // If array have leading number (ex: 1[{answer: blabla}])
+                // Remove leading number and parse into json
+                if (!response.data.startsWith('[')){
+                  dataStr = response.data.substring(response.data[0]);
+                  const data = JSON.parse(dataStr);
+                  setChatLog(prevLog => [...prevLog, { user: "gpt", message: data[0]["answer"]}]);
+                  console.log("Get response from parsing leading number");
+                }
+                
+                // Array map more than 1, need to combined first (update question)
+                else {
+                  const combinedArray = response.data.replace(/\]\[/g, ',');
+                  const parsedArray = JSON.parse(combinedArray);
+                  setChatLog(prevLog => [...prevLog, { user: "gpt", message: parsedArray[0]["answer"]}]);
+                  console.log("Get response from parsing more than 1 array map");
+                }
+              }
+
+              else { // Valid array, directly retrieve the data
+                setChatLog(prevLog => [...prevLog, { user: "gpt", message: response.data[0]["answer"]}]);
+                console.log("Get Response from direct valid array map");
+            }
           }
         }
       }
-    }
-    else{
-      console.log("No question found in database");
-    }
+      else{
+        console.log("No question found in database");
+      }
     })
     .catch(error => {
       console.log(error);
     });
-  }
+    await new Promise(resolve => setTimeout(resolve, 1500));
+  };
+}
 
   function clearLog(){
     setChatLog([]);
